4a08b3109f6b8d17e6dfac9a8101f0a4998251bf
mjaow1233 <nilsson.daniel1@gmail.com>
2025-11-04 14:19:24 +0100

    fixade problem i länkandet

--- js/pages/start.js
+++ js/pages/start.js
@@ -3,6 +3,9 @@ import clubInfoAndEvents from "../utils/club-info-and-events.js";
 export default async function start() {
   const eventHtml = await clubInfoAndEvents();
   return `
+        <section class="hero">
+        <img src="images/logo2.png" alt="Hero Image">
+        </section>
     <h1>Alla kommande events på Gala</h1>
     <p>Gala är en samlingsplats för olika musikklubbar.</p>
     ${eventHtml}
