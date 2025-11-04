2ab77959a246a436832cd9e09fada2c22f5a38a8
mjaow1233 <nilsson.daniel1@gmail.com>
2025-11-04 15:13:54 +0100

    ändrade outputten från clubinfo till att skapa eventcards.  Formaterade lite css.

--- js/pages/start.js
+++ js/pages/start.js
@@ -1,13 +1,24 @@
 import clubInfoAndEvents from "../utils/club-info-and-events.js";
 
 export default async function start() {
-  const eventHtml = await clubInfoAndEvents();
+  const eventsHtml = await clubInfoAndEvents();
+
   return `
-        <section class="hero">
-        <img src="images/logo5.png" alt="Hero Image">
-        </section>
-    <h1>Alla kommande events på Gala</h1>
-    <p>Gala är en samlingsplats för olika musikklubbar.</p>
-    ${eventHtml}
+    <section class="hero">
+      <img src="images/logo5.png" alt="heroimage">
+    </section>
+
+    <section class="upcoming">
+      <h2>Upcoming Events</h2>
+      <div class="events-grid">
+        ${eventsHtml}
+      </div>
+    </section>
+
+    <section class="clubs">
+      <a href="#jazz-klubben" class="club-btn">Jazz</a>
+      <a href="#piano-klubben" class="club-btn">Piano Club</a>
+      <a href="#add-club" class="club-btn add">+ Your Club</a>
+    </section>
   `;
 }
--- js/utils/club-info-and-events.js
+++ js/utils/club-info-and-events.js
@@ -1,31 +1,16 @@
-export default async function clubInfoAndEvents(clubId) {
-  let name = '', description = '';
-  // if there is a clubId -> fetch the info about the club
-  // and calculate the correct url for fetching filtered events
-  let url = 'http://localhost:3000/events';
-  if (clubId) {
-    const { name: clubName, description: clubDescription } =
-      await (await fetch('http://localhost:3000/clubs/' + clubId)).json();
-    name = clubName;
-    description = clubDescription;
-    url += '?clubId=' + clubId;
-  }
-  const events =
-    await (await fetch(url)).json();
-  // return html
-  return `
-    <h1>${name}</h1>
-    <p>${description}</p>
-    <h2>Events</h2>
-    ${events
-      .toSorted((a, b) => a.date > b.date ? 1 : -1)
-      .map(({ date, name, description }) => `
-        <article class="event">
-          <h3>${name} ${date}</h3>
-          <p>${description}</p>
-        </article>
-      `)
-      .join('')
-    }
-  `;
+export default async function clubInfoAndEvents() {
+  const res = await fetch("http://localhost:3000/events");
+  const events = await res.json();
+
+  return events
+    .map(
+      ({ date, name, description }) => `
+      <article class="event-card">
+        <h3>${name}</h3>
+        <p class="date">${new Date(date).toLocaleDateString()}</p>
+        <p>${description}</p>
+      </article>
+    `
+    )
+    .join("");
 }
