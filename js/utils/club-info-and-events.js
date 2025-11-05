9da9f04018747885739d3873e20e87642bb7eb01
Dalmar <dalmar.aliabdi10@gmail.com>
2025-11-05 15:23:00 +0100

    update to dev

--- js/utils/club-info-and-events.js
+++ js/utils/club-info-and-events.js
@@ -1,29 +1,16 @@
-export default async function clubInfoAndEvents(clubId) {
-  let name = '', description = '';
-  let url = 'http://localhost:3000/events';
-  if (clubId) {
-    const { name: clubName, description: clubDescription } =
-      await (await fetch('http://localhost:3000/clubs/' + clubId)).json();
-    name = clubName;
-    description = clubDescription;
-    url += '?clubId=' + clubId;
-  }
-  const events =
-    await (await fetch(url)).json();
+export default async function clubInfoAndEvents() {
+  const res = await fetch("http://localhost:3000/events");
+  const events = await res.json();
 
-  return `
-    <h1>${name}</h1>
-    <p>${description}</p>
-    <h2>Events</h2>
-    ${events
-      .toSorted((a, b) => a.date > b.date ? 1 : -1)
-      .map(({ date, name, description }) => `
-        <article class="event">
-          <h3>${name} ${date}</h3>
-          <p>${description}</p>
-        </article>
-      `)
-      .join('')
-    }
-  `;
+  return events
+    .map(
+      ({ date, name, description }) => `
+      <article class="event-card">
+        <h3>${name}</h3>
+        <p class="date">${new Date(date).toLocaleDateString()}</p>
+        <p>${description}</p>
+      </article>
+    `
+    )
+    .join("");
 }
