c941d5b303a87deb27ebe6a4eaa614c36278d53d
dalmaraliabdi10-art <dalmar.aliabdi10@gmail.com>
2025-11-05 11:20:19 +0100

    Delete Import

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 import start from './pages/start.js';
-import jazzClub from './pages/jazz-klubben.js';
-import metalClub from './pages/metal-klubben.js';
 import balettKlubben from './pages/balett-klubben.js';
 // Our menu: label to display in menu and 
 // function to run on menu choice
 const menu = {
   "start": { label: 'Start', function: start },
-  "jazz-klubben": { label: 'Jazz-klubben', function: jazzClub },
-  "metal-klubben": { label: 'Metal-klubben', function: metalClub },
   "balett-klubben": { label: 'Balettklubben', function: balettKlubben }
 };
 
