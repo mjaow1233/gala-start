847925d94f47171b4dcd105dfcefda56a6115e8f
dalmaraliabdi10-art <dalmar.aliabdi10@gmail.com>
2025-11-05 11:25:32 +0100

    Download files from Dev

---
+++ js/pages/about.js
@@ -0,0 +1,24 @@
+import clubInfoAndEvents from "../utils/club-info-and-events.js";
+
+export default async function about() {
+  const eventsHtml = await clubInfoAndEvents();
+
+  return `
+    <section class="hero">
+      <img src="images/logo16.png" alt="HeroImage">
+    </section>
+
+    <section class="upcoming">
+      <h2>Upcoming Events</h2>
+      <div class="events-grid">
+        ${eventsHtml}
+      </div>
+    </section>
+
+    <section class="clubs">
+      <a href="#jazz-klubben" class="club-btn">Jazz</a>
+      <a href="#piano-klubben" class="club-btn">Piano Club</a>
+      <a href="#add-club" class="club-btn add">+ Your Club</a>
+    </section>
+  `;
+}
