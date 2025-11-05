75343b67d07712ad47ecccf532b406fbd302c22d
Dalmar <dalmar.aliabdi10@gmail.com>
2025-11-05 15:34:35 +0100

    Add balett-klubben to Clubmenu

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ import events from './pages/events.js';
 import createEvent from './pages/create-event.js';
 import clubs from './pages/clubs.js';
 import bookEvent from './pages/book-event.js';
-
+import balettKlubben from './pages/balett-klubben.js';
 
 const isAdmin = true;
 
 const Clubmenu = {
   "jazz-klubben": { label: 'Jazz-klubben', function: jazzClub },
   "hamze-klubben": { label: 'hamze-klubben', function: hamzeClub }
-
+  "balett-klubben": { label: 'Balettklubben', function: balettKlubben }
 
 };
 const menu = {
