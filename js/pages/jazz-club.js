dbb5c31ba5c86c4c4e3ca7a066a316c42c6a53a1
mjaow1233 <nilsson.daniel1@gmail.com>
2025-11-13 13:37:40 +0100

    ordnat med css

--- js/pages/jazz-club.js
+++ js/pages/jazz-club.js
@@ -1,18 +1,48 @@
-import clubInfoAndEvents from "../utils/club-info-and-events.js";
 
 export default async function jazzClub() {
-  const eventsHtml = await clubInfoAndEvents();
+  document.body.classList.remove(...document.body.classList);
+  document.body.classList.add("jazzClub");
 
-  return `
+  const res = await fetch("http://localhost:3000/events");
+  const events = await res.json();
+
+
+
+  const filteredEvents = events
+    .filter((event) => event.clubId === "1")
+    .map(
+      ({ date, name, description }) => `
+      <article class="jazzevents">
+        <h3>${name}</h3>
+        <p class="date">${new Date(date).toLocaleDateString()}</p>
+        <p>${description}</p>
+      </article>
+    `
+    )
+    .join("");
+
+
+  const html = `
     <section class="hero">
-      <img src="images/jazz2.png" alt="HeroImage">
+      <video autoplay muted loop playsinline class="hero-video" id="heroVideo">
+        <source src="videos/jazz4.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </section>
+      <section class="jazzevents">
+    <h3>Upcoming events</h3>
+    <div class="events-grid">${filteredEvents}</div>
+</section>
 
-    <section class="upcoming">
-      <h2>Upcoming Events</h2>
-      <div class="grid">
-        ${eventsHtml}
-      </div>
-    </section>
+</ul>   
+
+   </div>
+</div>
   `;
+
+  return html;
 }
+
+
+
+
