71cab18b53e5a0b3cca16bce3369f615ad54b2b2
ChloeNkm <nakamanyachloe4@gmail.com>
2025-11-13 18:58:50 +0100

    Updated about page

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,27 +1,26 @@
-import clubInfoAndEvents from "../utils/club-info-and-events.js";
 
 export default async function about() {
-  const eventsHtml = await clubInfoAndEvents();
-
   return `
-    <section class="hero">
-      <img src="images/logo16.png" alt="HeroImage">
+ <div class="about_content">
+    <section class="about_img">
+      <img src="images/about.png" alt="About Image">
     </section>
 
-    <section class="upcoming">
-      <h2>Upcoming Events</h2>
-      <div class="events-grid">
-        ${eventsHtml}
-      </div>
-    </section>
 
-    <section class="clubs">
-      <a href="#jazzClub" class="club-button" id="jazz-btn">Jazz</a>
-      <a href="#pianoClub" class="club-button" id="piano-btn">Piano Club</a>
-      <a href="#djClub" class="club-button" id="dj-btn">DJ Club</a>
-      <a href="#operaClub" class="club-button" id="opera-btn">Opera Club</a>
-      <a href="#ballet-club" class="club-button" id="ballet-btn">Ballet Club</a>
-      <a href="#add-club" class="club-button" id="add-club-btn">+ Your Club</a>
-    </section>
+    <h1>About Gala Emporium</h1>
+   <p class="info">Gala Emporium is the place where culture, music, and performance meet under one roof. Here you can wander from the soulful sounds of jazz to the elegance of piano recitals, be swept away by the drama 
+   of opera, lose yourself in the energy <br> of a DJ set, or marvel at the grace of ballet. It is a space designed for discovery, where every evening offers<br> a new experience and every performance tells its own story.<br>
+   <br>Located in the heart of Stockholm, Gala Emporium has become a gathering point for those who 
+   love the arts and seek inspiration in live performance. Whether you come to celebrate, to connect, or simply to enjoy, you'll find a welcoming atmosphere and unforgettable moments waiting for you.<br>
+  <br>For more information, reach us via our contact information down below. We look forward to sharing the magic of Gala Emporium with you!</p>
+    
+<h3>Contact us!</h3> 
+<ul class="contact-us">
+  <li><strong>Location:</strong> Storgatan 3 Gamla Stan, Stockholm</li>
+  <li><strong>Phone:</strong> +46(0)40 123 456</li>
+  <li><strong>Email:</strong> info@galaemporium.se</li>
+</ul>
+    </div>
+
   `;
 }
